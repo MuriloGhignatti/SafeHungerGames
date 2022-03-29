3e4872c22a478590c03187d1e9651af63f741d8a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-29 16:59:22 -0300

    Creating the main class

---
+++ src/main/kotlin/SafeHungerGames.kt
@@ -0,0 +1,15 @@
+import org.bukkit.plugin.java.JavaPlugin
+
+class SafeHungerGames: JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onLoad() {
+        super.onLoad()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
